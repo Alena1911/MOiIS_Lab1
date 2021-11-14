28a3a5c8a88b98e2dd847c25bc86cf0bbf800df8
Alena <alena-236@yandex.ru>
2021-11-14 21:58:22 +0300

    Добавила комментарии

--- 1_1/salesCSV.py
+++ 1_1/salesCSV.py
@@ -7,11 +7,15 @@ outputFile = 'output.csv'
 if __name__ == '__main__':
     pdObj = pd.read_json(inputFile)
     table = [['item', 'country', 'year', 'sales']]
+    # идём по товарам
     for i, row in pdObj.iterrows():
         item = row['item']
         lpd = pd.DataFrame(eval(f"[{row['sales_by_country']}]"))
+        # идём по странам
         for country, data in lpd.items():
+            # идём по годам
             for year in data[0].items():
+                # заполняем массив для .csv файла
                 table.append([item, country, year[0], year[1]])
 
     csvFile = open(outputFile, 'w', newline='')
